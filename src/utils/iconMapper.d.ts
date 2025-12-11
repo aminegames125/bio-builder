b823e21c7ae82381125b7000511187b5042baa47
OrangeFoxTrees <aminegames125@gmail.com>
2025-12-11 11:04:31 +0100

    chore(types): add declaration file for iconMapper.js to fix TS7016

---
+++ src/utils/iconMapper.d.ts
@@ -0,0 +1,4 @@
+export declare const ICON_MAP: Record<string, string>;
+export declare function mapIconName(friendlyName: string): string | null;
+export declare function getAvailableIconNames(): string[];
+export default ICON_MAP;
